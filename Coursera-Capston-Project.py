c026beece44fe9a0d1857ecf348170c7fd0c7ce8
Federico Mancini <federico@Federicos-MBP.lan>
2020-02-24 09:22:39 +0100

    adds a python script that imports libreries and print a message

---
+++ Coursera-Capston-Project.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This Notebook is mainly used in the Coursera Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print ("Hello Capstone Project Course!")
+
